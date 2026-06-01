99840aba3cb709f7e2699aa3ed0a82605ee663c9
Arlaannnd <qoulanahmad9@gmail.com>
2026-06-01 07:53:45 +0700

    Delete Desktop Version directory

--- Desktop Version/database/supabaseclient.js
+++
@@ -1,7 +0,0 @@
-// supabaseClient.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-
-const SUPABASE_URL = 'https://aqrbqoaicqzwehzuxckf.supabase.co'  // ganti dengan URL kamu
-const SUPABASE_KEY = 'sb_publishable_HZfl7aJTc5Ex1fmV-ktN8Q_7XRwxnx5'      // jangan hardcode di file ini idealnya
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
